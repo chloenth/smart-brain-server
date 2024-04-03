bb75cbbda1ef0c163891c68a9f4326226aea8c4f
Huong <huongnguyen.nth1709@gmail.com>
2024-04-03 18:57:44 +0300

    deploy server

---
+++ controllers/image.js
@@ -0,0 +1,63 @@
+const returnClarifyRequestOptions = (imageUrl) => {
+  // Your PAT (Personal Access Token) can be found in the Account's Security section
+  const PAT = process.env.PAT;
+  // Specify the correct user_id/app_id pairings
+  // Since you're making inferences outside your app's scope
+  const USER_ID = process.env.USER_ID;
+  const APP_ID = process.env.APP_ID;
+  // Change these to whatever model and image URL you want to use
+  const IMAGE_URL = imageUrl;
+
+  const raw = JSON.stringify({
+    user_app_id: {
+      user_id: USER_ID,
+      app_id: APP_ID,
+    },
+    inputs: [
+      {
+        data: {
+          image: {
+            url: IMAGE_URL,
+          },
+        },
+      },
+    ],
+  });
+
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      Authorization: 'Key ' + PAT,
+    },
+    body: raw,
+  };
+
+  return requestOptions;
+};
+
+const handleImage = (db) => (req, res) => {
+  const { id, imageUrl } = req.body;
+
+  fetch(
+    'https://api.clarifai.com/v2/models/face-detection/outputs',
+    returnClarifyRequestOptions(imageUrl)
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      const clarifaiFace =
+        data.outputs[0].data.regions[0].region_info.bounding_box;
+      db('users')
+        .returning('*')
+        .where({ id })
+        .increment('entries', 1)
+        .returning('entries')
+        .then((entries) =>
+          res.json({ entries: entries[0].entries, clarifaiFace })
+        )
+        .catch((err) => res.status(400).json('unable to get entries'));
+    })
+    .catch((err) => res.status(400).json('error getting face detection'));
+};
+
+module.exports = { handleImage };
